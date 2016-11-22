f177dc372b5122dc9af1d1d75865359e76fded03
christina-hammer <christinaelizabethhammer@gmail.com>
2016-11-21 21:19:57 -0500

    Update incident_generator_class.py
    added in testing, comments, standardized variable stylization

--- incident_generator_class.py
+++ incident_generator_class.py
@@ -1,160 +1,172 @@
-import json, pytest
-import db_initial
+#classes centered around processing data taken from the database JSON, storing it, and wriitng it to a JSON for the front end to parse
+#written for RPI Incident Map
+
+import sys
+import json
+import os
+#import pytest
+#import db_initial
 from datetime import date
 from datetime import time
 
+#class to hold location information within the Incident Class
+#initilization requires a location name (building or street) and the appropriate location grouping
 class Location:
     def __init__(self, name, group):
-        self.lat = 42.727
-        self.long = -73.676
-        self.location_name = name
-        self.location_group = group
+        self.lat = 42.730 #default coordinates to be used if none found, center of campus
+        self.long = -73.681 #default coordinates to be used if none found, center of campus
+        self.locationName = name #building or street name
+        self.locationGroup = group #pre-determined region of campus
 
     def setCoords(self, _lat, _long):
         self.lat = _lat
         self.long = _long
 
-
-##this will be initialized using strings and datetime objects created from the information in the mongo db entries
+#Incident class to hold information for filtering/displaying individual incidents
+#initialization requires strings for eventNum and incidentType, a location object, and date objects for dateReported and dateOccurred
 class Incident:
-    def __init__(self, _event_num, _incident_type, _location, _date_occurred, _date_reported):
-        self.event_num = _event_num #int
-        self.incident_type = _incident_type #string
+    def __init__(self, _eventNum, _incidentType, _location, _dateOccurred, _dateReported):
+        self.eventNum = _eventNum #string
+        self.incidentType = _incidentType #string
         self.location = _location #location description
 
         ##just use python built-in date objects for these::
-        self.date_reported = _date_reported #datetime.date
-        self.date_occurred = _date_occurred #datetime.date
+        self.dateReported = _dateReported #datetime.date
+        self.dateOccurred = _dateOccurred #datetime.date
 
+#LocationData class for storing data used for populating Location objects for use within an Incident object
+#known, static set of buildings and landmarks are grouped into location groups based on subjective campus significance, hence the hard-coding
 class LocationData():
     def __init__(self):
 
-        self.north = {"GYMNASIUM, '87", "CAMPUS PARKING, NORTH LOT", "E-COMPLEX", "COLONIE APARTMENTS", "HEFFNER ALUMNI HOUSE", "J-BUILDING", "SAGE AVENUE", "BLITMAN RESIDENCE COMMONS"}
+        self.__north = {"GYMNASIUM, '87", "CAMPUS PARKING, NORTH LOT", "E-COMPLEX", "COLONIE APARTMENTS", "HEFFNER ALUMNI HOUSE", "J-BUILDING", "SAGE AVENUE", "BLITMAN RESIDENCE COMMONS", "COLONIE APTS., BUILDING A"}
 
-        self.east = {"EAST CAMPUS ATHLETIC VILLAGE", "HOUSTON FIELD HOUSE", "BURDETT AVENUE RESIDENCE", "DAVISON HALL", "NASON HALL", "CROCKETT HALL", "BARTON HALL", "RENSSELAER UNION", "BRAY HALL" "RAHPS - ALBRIGHT", "RAHPS-B", "COMMONS DINING HALL (FRES", "COMMONS PARKING", "Robinson Pool", "MUELLER CENTER", "QUADRANGLE DORMS", "DEPARTMENT OF PUBLIC SAFETY", "NED HARKNESS ATHLETIC FIE", "STACWYCK - MCGIFFERT", "Sherry Road", "BRYCKWYCK APARTMENTS", "WARREN HALL", "CARY HALL", "BRINSMADE TERRACE", "Sharp Hall", "RENSSELAER CRITICAL FACIL", "NUGENT HALL", "STACWYCK-ROUSSEAU", "STACWYCK - WILTSIE", "PUBLIC SAFETY OFFICE", "H- BUILDING"}
+        self.__east = {"EAST CAMPUS ATHLETIC VILLAGE", "HOUSTON FIELD HOUSE", "BURDETT AVENUE RESIDENCE", "DAVISON HALL", "NASON HALL", "CROCKETT HALL", "BARTON HALL", "RENSSELAER UNION", "BRAY HALL" "RAHPS - ALBRIGHT", "RAHPS-B", "COMMONS DINING HALL (FRES", "COMMONS PARKING", "SPORTS CENTER AND ROBISON", "MUELLER CENTER", "QUADRANGLE DORMS", "DEPARTMENT OF PUBLIC SAFETY", "NED HARKNESS ATHLETIC FIE", "STACWYCK - MCGIFFERT", "Sherry Road", "BRYCKWYCK APARTMENTS", "WARREN HALL", "CARY HALL", "BRINSMADE TERRACE", "Sharp Hall", "RENSSELAER CRITICAL FACIL", "NUGENT HALL", "STACWYCK-ROUSSEAU", "STACWYCK - WILTSIE", "PUBLIC SAFETY OFFICE", "H- BUILDING"}
+        self.__off = {"OTHER OFF-CAMPUS LOCATION", "BEMAN LANE", "SHERRY ROAD"}        
+        self.__west = {"POLYTECH APARTMENTS", "CITY STATION", "CAMPUS PARKING, WEST LOT", "EMPAC BUILDING", "WEST HALL"}        
+        self.__south = {"MOE'S SOUTHWEST GRILL"}        
+        self.__academic = {"RUSSELL SAGE LABORATORY", "COGSWELL LABORATORY", "GREENE BUILDING", "JONSSON ENGINEERING CENTE", "ACADEMY HALL", "BIOTECH"}        
+        self.__defaultGroup = "RPI"
 
-        self.off = {"OTHER OFF-CAMPUS LOCATION", "BEMAN LANE", "SHERRY ROAD"}
-        self.west = {"POLYTECH APARTMENTS", "CITY STATION", "CAMPUS PARKING, WEST LOT", "EMPAC BUILDING", "WEST HALL"}
-        self.south = {"MOE'S SOUTHWEST GRILL"}
-        self.academic = {"RUSSELL SAGE LABORATORY", "COGSWELL LABORATORY", "GREENE BUILDING", "JONSSON ENGINEERING CENTE", "ACADEMY HALL", "BIOTECH"}
-        self.default_group = "RPI"
-
-    def locationGroup(self, loc):
-        assert loc != ""
-
-        if(loc in self.north): return "North"
-        elif(loc in self.off): return "Off Campus"
-        elif(loc in self.west): return "West"
-        elif(loc in self.south): return "South"
+    def locationGroup(self, loc):        
+        if(loc in self.__north): return "North"
+        elif(loc in self.__off): return "Off Campus"
+        elif(loc in self.__west): return "West"
+        elif(loc in self.__south): return "South"
+        elif(loc in self.__east): return "East"
+        elif(loc in self.__academic): return "Academic"
         else:
-            return self.default_group
+            return self.__defaultGroup
 
-
+#JsontoIncident class uses a given (or default) date range to select an approprite set of month/year tagged JSON files to create
+#a list of incident objects
 class JsonToIncident:
     def __init__(self):
-        self.begin_date = date(2016, 10, 1)
-        self.end_date = date(2016, 10, 31)
+        self.__beginDate = date(date.today().year, date.today().month, 1)
+        self.__endDate = date(date.today().year, date.today().month, 30)
 
-        self.location_data = LocationData()
-        self.json_files = {}
+        self.__locationData = LocationData()
+        self.__jsonFiles = {}
 
+    #assign dicitonary of {(month, year), "filename"} entries to refer to for parsing grouped by months
     def setJsonFiles(self, files):
-        self.json_files = files
+        self.__jsonFiles = files
         return 0
-
+        
     #returns list of dictionaries for months matching date range
-    def getJsonDic(self):
-        json_dic_list = [] #list of dictionaries with all json files of specified date range
-
-        assert isinstance(self.begin_date, date())
-        assert isinstance(self.end_date, date())
-
-        m = self.begin_date.month
-        y = self.begin_date.year
+    def __getJsonDic(self):
+        jsonDicList = [] #list of dictionaries with all json files of specified date range
+        
+        m = self.__beginDate.month
+        y = self.__beginDate.year
 
         while (y <= date.today().year):
-            time_pair = (m, y)
-            if time_pair in self.json_files:
-                with open(self.json_files[time_pair]) as f:
+            timePair = (m, y)
+            if timePair in self.__jsonFiles:
+                with open(self.__jsonFiles[timePair]) as f:
                     incid = json.load(f) #list of dictionaries containing json info
-                json_dic_list = json_dic_list + incid
+                jsonDicList = jsonDicList + incid
 
             m = m+1
             if (m == 12):
                 m = 1
                 y = y + 1
 
-            if (y > self.end_date.year): break
-            elif (m > self.end_date.month and y == self.end_date.year): break
+            if (y > self.__endDate.year): break
+            elif (m > self.__endDate.month and y == self.__endDate.year): break
 
-        return json_dic_list
-
-    def assignLocationGroup(self, location):
+        return jsonDicList
+    
+    #helper function for creating location objects for creating incident objects
+    def __assignLocationGroup(self, location):
         location = location.upper()
-        return self.location_data.locationGroup(location)
+        return self.__locationData.locationGroup(location)
 
-
+    #main method for creation of Incident objects from the JSON file
     def createIncidents(self):
         incidents = {}
 
-        incid = self.getJsonDic()
-        assertTrue(len(incid) > 0)
-        d = self.begin_date
+        incid = self.__getJsonDic()
+        
+        d = self.__beginDate
         i = 0
 
         while (i < len(incid)):
             inc = incid[i]
-            inc_date_occ = inc['event #'].split("-")
-            date_occurred = date(2000 + int(inc_date_occ[0]), int(inc_date_occ[1]), int(inc_date_occ[2]) )
+            incDateOcc = inc['event #'].split("-")
+            dateOccurred = date(2000 + int(incDateOcc[0]), int(incDateOcc[1]), int(incDateOcc[2]) )
 
-            if(date_occurred >= self.end_date):
+            if(dateOccurred >= self.__endDate):
                 break
-            elif (date_occurred >= self.begin_date and not(inc['event #'] in incidents) ):
+            elif (dateOccurred >= self.__beginDate and not(inc['event #'] in incidents) ):
                 #print(inc)
                 inc_date_rep = inc['date reported'].split(" ", 1)
                 inc_date_rep = inc_date_rep[0].split("/")
-                date_reported = date(2000 + int(inc_date_rep[2]), int(inc_date_rep[0]), int(inc_date_rep[1]))
+                dateReported = date(2000 + int(inc_date_rep[2]), int(inc_date_rep[0]), int(inc_date_rep[1]))
 
-                loc = Location(inc['location'], self.assignLocationGroup(inc['location']))
-
+                loc = Location(inc['location'], self.__assignLocationGroup(inc['location']))
+                
                 loc.setCoords(inc['coordinates'][0], inc['coordinates'][1])
-                inc_obj = Incident(inc['event #'], incid[i]['incident'], loc, date_occurred, date_reported)
+                inc_obj = Incident(inc['event #'], incid[i]['incident'], loc, dateOccurred, dateReported)
 
                 incidents[inc['event #']] = inc_obj
             i = i + 1
 
-        print(len(list(incidents.values() )))
+        #print(len(list(incidents.values() )))
         return list(incidents.values() )
 
-    def setDateRange(self, _begin, _end):
-        assertTrue(_begin <= _end)
-        self.begin_date = _begin
-        self.end_date = _end
+    #set the date range for the incidents to be used to be created
+    def setDateRange(self, begin, end):
+        
+        self.__beginDate = begin
+        self.__endDate = end
 
+#IncidentToJson class has the responsibility of taking the set of Incident objects being stored in the Incident Cache and creating a Json for use with the front-end
 class IncidentToJson():
-
     def __init__(self):
-        pass
+        pass #currently takes no parameters but wanted to write functionality into class for extensiblilty purposes.
 
-    def dumpJSON(self, file_obj, incid_dic_list):
+    #helper function handles writing to JSON 
+    def __dumpJSON(self, file_obj, incidDicList):
         #docs_list = list(incid_dic)
-        json_docs = json.dumps(incid_dic_list, indent = 4)
-        file_obj.write(json_docs)
+        jsonDocs = json.dumps(incidDicList, indent = 4)
+        file_obj.write(jsonDocs)
 
+    #creates JSON object for each incident object and writes to a JSON file whose name it returns
     def createJson(self, incidents):
 
-        fname = 'incidents.json'
-        incid_dic_list = []
+        fname = 'incidents_.json'
+        incidDicList = []
 
-        with open(fname, 'w') as json_file:
+        with open(fname, 'w') as jsonFile:
             for incid in incidents:
+                #create dictionary out of incident object as intermediate step in making JSON object
+                incid_dic = {'event #': incid.eventNum, 'incidentType': incid.incidentType, 'location': incid.location.locationName, 'location group': incid.location.locationGroup, 'latitude': incid.location.lat, "longitude": incid.location.long, 'date reported': incid.dateReported.isoformat(), 'dateOccurred': incid.dateOccurred.isoformat()}
 
-                incid_dic = {'event #': incid.event_num, 'incident_type': incid.incident_type, 'location': incid.location.location_name, 'location group': incid.location.location_group, 'latitude': incid.location.lat, "longitude": incid.location.long, 'date reported': incid.date_reported.isoformat(), 'date_occurred': incid.date_occurred.isoformat()}
-
-                incid_dic_list.append(incid_dic)
-
-            self.dumpJSON(json_file, incid_dic_list)
-        json_file.close()
+                incidDicList.append(incid_dic)
+                
+            self.__dumpJSON(jsonFile, incidDicList)
+        jsonFile.close()
 
         return fname
 
@@ -164,71 +176,90 @@ class IncidentCache:
     #init will take in date objects made outside of the cache class in the main function upon start up reflecting the range of dates in the database that we want to pull incidents from
     def __init__(self):
         self.incidents = [] #list of incident objects
-        self.begin_date = date(date.today().year, date.today().month-1, 1) #default starting last month
-        self.end_date = date.today()
+        self.__beginDate = date(date.today().year, date.today().month-1, 1) #default starting last month
+        self.__endDate = date.today()
+        #create own instance of JasonToIncident class to handle populating cache
+        self.__jToI = JsonToIncident() 
 
-        self.JtoI = JsonToIncident()
-
+    #called to interface with use
     def assignJsonFiles(self, files):
-        r = self.JtoI.setJsonFiles(files)
-        assert r == 0
+        self.__jToI.setJsonFiles(files)
 
-    def setCacheByDate(self, _begin_date, _end_date):
-        assert _begin_date < _end_date
-
+    #public-interfacing function to handle cache-date setting and population logic
+    def setCacheByDate(self, beginDate, endDate):
 
         if (len(self.incidents) == 0):
-            self.begin_date = _begin_date
-            self.end_date = _end_date
-            self.fullyPopulateCache()
-        elif (_begin_date == self.begin_date and _end_date == self.end_date):
+            #if list of incidents is empty, populate with all from begin - end
+            self.__beginDate = beginDate
+            self.__endDate = endDate
+            self.__fullyPopulateCache()
+        elif (_beginDate == self.__beginDate and _endDate == self.__endDate):
+            #if new dates are identical to dates already assigned no work need be done
             return 0
         else:
-            self._setCacheByDate(_begin_date, _end_date)
+            #new begin and end dates will result in potentially different cache contents
+            self.__setCacheByDate(beginDate, endDate)
         return 0
 
-    def fullyPopulateCache(self):
-        assert self.begin_date != None
-        assert self.end_date != None
-        assert self.begin_date <= self.end_date
+    #for when Cache is empty and needs to be newly occupied and create full set o 
+    def __fullyPopulateCache(self):
+        
+        self.__jToI.setDateRange(self.__beginDate, self.__endDate)
+        self.incidents = self.__jToI.createIncidents()
 
-        self.JtoI.setDateRange(self.begin_date, self.end_date)
-        self.incidents = self.JtoI.createIncidents()
-
-    def _setCacheByDate(self, new_begin, new_end):
-
-        assert new_begin <= date.today()
-        assert new_end <= date.today()
+    #occupy list of cache objects when there are already Incident objects occupying cache
+    #seeks to avoid re-creating Incident objets already stored in cache that are still within newly specified dates
+    def __setCacheByDate(self, new_begin, new_end):
 
         new_incidents = []
-
+        
+        #add all of the Incidents from the old cache that fulfill the new dates into what
+        #will be the new list
         for incid in self.incidents:
-            if (incid.date_occurred <= new_end and incid.date_occurred >= new_begin):
+            if (incid.dateOccurred <= new_end and incid.dateOccurred >= new_begin):           
                 new_incidents.append(incid)
+        
+        #find and add to list incidents that took place before the current beginning date
+        #but after/during the new benning date
+        if (new_begin < self.beginDate):
+            self.__jToI.setDateRange(new_begin, self.beginDate)
+            new_incidents = new_incidents + self.__jToI.createIncidents()
 
-        if (new_begin < self.begin_date):
-            self.JtoI.setDateRange(new_begin, self.begin_date)
-            new_incidents = new_incidents + self.JtoI.createIncidents()
-
-        if (new_end > self.end_date):
-            self.JtoI.setDateRange(self.end_date, new_end)
-            new_incidents = new_incidents + self.JtoI.createIncidents()
-
-        self.begin_date = new_begin
-        self.end_date = new_end
+        #find and add to list incidents that took place after the current ending date 
+        #but before/during the new end date
+        if (new_end > self.endDate):
+            self.__jToI.setDateRange(self.endDate, new_end)
+            new_incidents = new_incidents + self.__jToI.createIncidents()
+        
+        self.__beginDate = new_begin
+        self.__endDate = new_end
 
         self.incidents = new_incidents
 
+#test to make sure that json file was succefully made and is not empty
+def testJsonSuccessfullyCreated(jsonOutputFile):   
+    
+    assert(os.path.isfile(jsonOutputFile))
+    assert(os.stat(jsonOutputFile).st_size > 0)
+    
+#test to be sure that cache is not empty before trying to make json from its contents    
+def testCachePopulated(cache):      
+    assert(len(cache.incidents) > 0)
+    
 
-
+#called by front end to create cache,  occupy, and create JSON for given month value
 def getCacheJsonByMonth(month):
-
-    assert db_initial.runDB() == 0
+    
+    db_initial.runDB()
     cache = IncidentCache()
 
     cache.assignJsonFiles(db_initial.filename())
+    cache.assignJsonFiles({(11,2016): "NOV_16.JSON"})
     r = cache.setCacheByDate(date(2016, month, 1), date(2016, month, 30))
-    assert r == 0
+    testCachePopulated(cache)
+    
     ItoJ = IncidentToJson()
-
-    return ItoJ.createJson(cache.incidents)
+    jsonFname = ItoJ.createJson(cache.incidents)
+    testJsonSuccessfullyCreated(jsonFname)
+    
+    return jsonFname
